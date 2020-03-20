d24db46ddbb7c53897b7ea615101280a5b1b9b32
Emir Méndez <mvdcreativo@gmail.com>
2020-03-20 19:00:31 -0300

    cambio de email

--- app/Http/Controllers/Api/MessageController.php
+++ app/Http/Controllers/Api/MessageController.php
@@ -42,7 +42,7 @@ class MessageController extends Controller
         $message->message = $request->message;
         $message->save();
         
-        $mail_destino = "sbonzini@picaportedesign.com";
+        $mail_destino = "veterinarianuevaera@gmail.com";
 
         Mail::to($mail_destino)->queue(new MessageContact($msg));
 
