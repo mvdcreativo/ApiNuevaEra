9599e6203d136f23064084002c3e4e82897bc3f2
unknown <mvdcreativo@gmail.com>
2020-06-29 02:48:36 -0300

    categorias with brands

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -19,7 +19,7 @@ class CategoryController extends Controller
     public function index()
     {
         //
-        return Category::all();
+        return Category::with('brands')->get();
     }
 
 
