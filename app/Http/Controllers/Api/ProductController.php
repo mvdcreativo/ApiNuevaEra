e9cc7c80b56d4faa231f093d3f933a702003847f
unknown <mvdcreativo@gmail.com>
2020-08-12 20:39:31 -0300

    guardar export faceboock

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -175,7 +175,8 @@ class ProductController extends Controller
 
 
     public function exportFaceboock(){
-        return Excel::download(new ProductFacebookExport, 'da_catalog_commerce_commented_template.csv');
+        // Storage::disk('public')->put('images/productos',  $img);
+        return Excel::store(new ProductFacebookExport, 'da_catalog_commerce_commented_template.csv', storage_path('storage/excel/exports'));//'da_catalog_commerce_commented_template.csv'
 
     }
 
