57a1f001658a5903942d7d3014392674e0a2dc4b
mvdcreativo <mvdcreativo@gmail.com>
2021-01-20 10:39:04 -0300

    prueba3

--- app/Http/Controllers/Api/Auth/SocialAuthController.php
+++ app/Http/Controllers/Api/Auth/SocialAuthController.php
@@ -38,22 +38,22 @@ class SocialAuthController extends Controller
         if($provider == "APPLE"){
             
             
-            if (!isset($request->response->email)){
-                $user_excist = User::where('email', $request->response->email)->first();
+            if ($request->response['email']){
+                $user_excist = User::where('email', $request->response['email'])->first();
                 ///si existe logeamos y devolvemos token
                 if($user_excist){
-                    $user_excist->user_apple = $request->response->user;
+                    $user_excist->user_apple = $request->response['user'];
                     $user_excist->save();
     
                     return $this->login($user_excist);
                 }else{
                 /// si no existe creamos usuario
                     $user = new User([
-                        'name'     => $request->response->givenName,
-                        'lastname' => $request->response->familyName,
-                        'email'    => $request->response->email,
+                        'name'     => $request->response['givenName'],
+                        'lastname' => $request->response['familyName'],
+                        'email'    => $request->response['email'],
                         'password' => bcrypt(Str::random(8)),
-                        'social_id' => $request->response->user,
+                        'social_id' => $request->response['user'],
                     ]);
                     $user->save();
     
@@ -63,6 +63,12 @@ class SocialAuthController extends Controller
                     
                 }
 
+            }else{
+                $user = User::where('user_apple', $request->response['user'])->first();
+                    
+                if($user){
+                    return $this->login($user);
+                };
             }
             return response()->json(['message' => 'Unauthorized'], 401);
         }
