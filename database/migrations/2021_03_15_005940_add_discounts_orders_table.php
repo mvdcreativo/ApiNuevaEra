94f563088d115ecbbc9cbfd01089bf36e7248f14
mvdcreativo <mvdcreativo@gmail.com>
2021-03-16 00:04:04 -0300

    migracion add_discount funcion rollback

--- database/migrations/2021_03_15_005940_add_discounts_orders_table.php
+++ database/migrations/2021_03_15_005940_add_discounts_orders_table.php
@@ -26,6 +26,9 @@ class AddDiscountsOrdersTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('order_product', function (Blueprint $table) {
+            $table->dropColumn('discount_user');
+            $table->dropColumn('discount_product');
+        });
     }
 }
