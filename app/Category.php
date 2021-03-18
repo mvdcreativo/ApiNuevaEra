dbae754d473cd41df097868d8fb9c7359960933f
mvdcreativo <mvdcreativo@gmail.com>
2021-03-18 16:35:07 -0300

    orden list pproduct

--- app/Category.php
+++ app/Category.php
@@ -20,7 +20,7 @@ class Category extends Model
 
     public function products()
     {
-        return $this->hasMany('App\Product');
+        return $this->hasMany('App\Product')->orderBy('id', 'desc');
     }
 
     public function brands()
